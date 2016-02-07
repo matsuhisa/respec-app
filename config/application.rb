b08a0b316be0ecdec76e99ebc1797063e0a66e18
matsuhisa <matsuhisa+github@gmail.com>
2016-02-07 23:46:06 +0900

    Add ja.yml

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module RspecApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.i18n.default_locale = :ja
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
