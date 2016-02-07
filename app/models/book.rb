14e428b4823bc1a98052785c42af8991c1619b55
matsuhisa <matsuhisa+github@gmail.com>
2016-02-07 23:46:48 +0900

    Use validates for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: { in: 2..10 }
+  validate :first_name_is_natsu
+
+  def first_name_is_natsu
+    if name.present? && name.split("").first != "夏"
+      errors.add(:name, :is_summer)
+    end
+  end
+
 end
