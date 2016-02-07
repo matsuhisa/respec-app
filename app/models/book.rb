3bc78e773e7af189fc2ea5acab22fd74e6122f60
matsuhisa <matsuhisa+github@gmail.com>
2016-02-08 00:34:46 +0900

    Add validate message

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveRecord::Base
   validates :name, length: { in: 2..10 }
   validate :first_name_is_natsu
 
+  private
   def first_name_is_natsu
     if name.present? && name.split("").first != "夏"
       errors.add(:name, :is_summer)
